bad28c2bb29adce5589d1b32cb9993eb42e4ba82
Evgenii Zaitsev <evgenii@cloudwalk.io>
2024-04-19 22:42:18 +0700

    test: rewrite PixCashier tests using a fixture, refactor, optimize

--- test-utils/misc.ts
+++ test-utils/misc.ts
@@ -1,11 +1,5 @@
-function countNumberArrayTotal(array: number[]) {
-  return array.reduce((sum: number, currentValue: number) => {
-    return sum + currentValue;
-  });
-}
-
 function createRevertMessageDueToMissingRole(address: string, role: string) {
   return `AccessControl: account ${address.toLowerCase()} is missing role ${role.toLowerCase()}`;
 }
 
-export { countNumberArrayTotal, createRevertMessageDueToMissingRole };
+export { createRevertMessageDueToMissingRole };

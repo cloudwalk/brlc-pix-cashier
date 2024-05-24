d52fc3e2e61cba1dc073a4fa8d42f02a86514dbc
yuriicw <yurii.pasternak@cloudwalk.io>
2024-05-24 10:54:01 +0200

    Update pix hooks interfaces

--- contracts/interfaces/IPixHook.sol
+++ contracts/interfaces/IPixHook.sol
@@ -1,8 +1,26 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.16;
+pragma solidity ^0.8.16;
 
+/**
+ * @title IPixHook interface
+ * @dev Interface for hook functions invoked by Pix Cashier contract.
+ * @author Cloudwalk Inc
+ */
 interface IPixHook {
-    /// @dev TODO
-    function onPixHook(uint256 hookIndex, bytes32 txId) external;
+    /**
+     * @dev Invoked by Pix Cashier contract during cash-in operations.
+     * @param hookIndex The index of the hook kind.
+     * @param txId The unique identifier of the transaction.
+     * @param hookFlags The flags indicating the hook types.
+     */
+    function onPixCashInHook(uint256 hookIndex, bytes32 txId, uint256 hookFlags) external;
+
+    /**
+     * @dev Invoked by Pix Cashier contract during cash-out operations.
+     * @param hookIndex The index of the hook kind.
+     * @param txId The unique identifier of the transaction.
+     * @param hookFlags The flags indicating the hook types.
+     */
+    function onPixCashOutHook(uint256 hookIndex, bytes32 txId, uint256 hookFlags) external;
 }
--- contracts/interfaces/IPixHookable.sol
+++ contracts/interfaces/IPixHookable.sol
@@ -1,42 +1,54 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.16;
+pragma solidity ^0.8.16;
 
 /**
  * @title PixHookable types interface
+ * @dev Interface defining types related to hookable functions.
+ * @author Cloudwalk Inc.
  */
 interface IPixHookableTypes {
     /**
      * @dev Possible types of hook functions that can be called by the contract during concrete actions.
-     *
-     * The possible values: TODO
      */
     enum HookKind {
-        CashInCommonBefore,        // 0
-        CashInCommonAfter,         // 1
-        CashInPremintBefore,       // 2
-        CashInPremintAfter,        // 3
-        CashOutRequestBefore,      // 4
-        CashOutRequestAfter,       // 5
-        CashOutConfirmationBefore, // 6
-        CashOutConfirmationAfter,  // 7
-        CashOutReversalBefore,     // 8
-        CashOutReversalAfter       // 9
+        CashInDefaultBefore,        // 0
+        CashInDefaultAfter,         // 1
+        CashInPremintBefore,        // 2
+        CashInPremintAfter,         // 3
+        CashOutRequestBefore,       // 4
+        CashOutRequestAfter,        // 5
+        CashOutConfirmationBefore,  // 6
+        CashOutConfirmationAfter,   // 7
+        CashOutReversalBefore,      // 8
+        CashOutReversalAfter        // 9
     }
 
-    /// @dev TODO
+    /**
+     * @dev Configuration struct for hooks.
+     * @param callableContract The address of the contract implementing hook functions.
+     * @param hookFlags Flags indicating which and how hook functions should be called.
+     */
     struct HooksConfig {
         address callableContract;
-        uint256 hookFlags; // TODO like (1 << CashInHookKind.CommonBefore) + (1 << CashInHookKind.CommonAfter) + ...
+        uint256 hookFlags;
     }
 }
 
 /**
  * @title PixHookable interface
- * @dev The interface of an addition to the PixCashier contract that allows to call hook functions during some actions.
+ * @dev Interface for adding hook functions to the PixCashier contract to enable calling hooks during specific actions.
+ * @author Cloudwalk Inc.
  */
 interface IPixHookable is IPixHookableTypes {
-    /// @dev TODO
+    /**
+     * @dev Emitted when cash-in hooks are registered.
+     * @param txId Transaction ID.
+     * @param newCallableContract Address of the new callable contract.
+     * @param oldCallableContract Address of the old callable contract.
+     * @param newHookFlags New hook flags.
+     * @param oldHookFlags Old hook flags.
+     */
     event CashInHooksRegistered(
         bytes32 indexed txId,
         address newCallableContract,
@@ -45,7 +57,14 @@ interface IPixHookable is IPixHookableTypes {
         uint256 oldHookFlags
     );
 
-    /// @dev TODO
+    /**
+     * @dev Emitted when cash-out hooks are registered.
+     * @param txId Transaction ID.
+     * @param newCallableContract Address of the new callable contract.
+     * @param oldCallableContract Address of the old callable contract.
+     * @param newHookFlags New hook flags.
+     * @param oldHookFlags Old hook flags.
+     */
     event CashOutHooksRegistered(
         bytes32 indexed txId,
         address newCallableContract,
@@ -54,7 +73,12 @@ interface IPixHookable is IPixHookableTypes {
         uint256 oldHookFlags
     );
 
-    /// @dev TODO
+    /**
+     * @dev Emitted when a hook is invoked.
+     * @param txId Transaction ID.
+     * @param hookKind Kind of hook that was invoked.
+     * @param callableContract Address of the contract where the hook function is implemented.
+     */
     event HookInvoked(
         bytes32 indexed txId,
         HookKind indexed hookKind,
@@ -62,12 +86,18 @@ interface IPixHookable is IPixHookableTypes {
     );
 
     /**
-     * @dev Registers cash-in hooks to call TODO
+     * @dev Registers cash-in hooks to call during specific actions.
+     * @param txId Transaction ID.
+     * @param newCallableContract Address of the contract implementing hook functions.
+     * @param newHookFlags Flags indicating which hook functions to call.
      */
     function registerCashInHooks(bytes32 txId, address newCallableContract, uint256 newHookFlags) external;
 
     /**
-     * @dev Registers cash-out hooks to call TODO
+     * @dev Registers cash-out hooks to call during specific actions.
+     * @param txId Transaction ID.
+     * @param newCallableContract Address of the contract implementing hook functions.
+     * @param newHookFlags Flags indicating which hook functions to call.
      */
     function registerCashOutHooks(bytes32 txId, address newCallableContract, uint256 newHookFlags) external;
 }
